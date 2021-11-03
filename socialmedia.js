7620b4c723fa101c53bcd1f39ac412ba2eced53d
Onakoya Kayode <75381388+onakoyakayode@users.noreply.github.com>
2021-11-03 08:40:56 +0100

    Added social media button

---
+++ socialmedia.js
@@ -0,0 +1,6 @@
+function socialMedia() {
+    const anchorTag = document.createElement('a')
+    anchorTag.href = 'https://sojioyawoye.com/pages/about-me/'
+    anchorTag.setAttribute("target", "_blank")
+    anchorTag.click()
+}
